bcf89729da7f3d67030ad3f6182de79ae68a2c5b
Uneeza Tukassar <116105019+Uneeza-02@users.noreply.github.com>
2026-05-01 13:16:36 +0500

    Updated Backend Code

--- Digital Marketing/Data/ApplicationDbContextFactory.cs
+++ Digital Marketing/Data/ApplicationDbContextFactory.cs
@@ -7,11 +7,16 @@ namespace Digital_Marketing.Data
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            optionsBuilder.UseSqlServer(
-                "Server=DESKTOP-IH9U8VD;Database=DigitalMarketingDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;"
-            );
+            optionsBuilder.UseSqlServer(connectionString);
 
             return new ApplicationDbContext(optionsBuilder.Options);
         }
